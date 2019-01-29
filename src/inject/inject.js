b4f79ccfdba41ba64e62a464a15d2f758fcdeb31
Tommaso A <tommaso.amici@me.com>
2019-01-29 16:04:30 +0100

    removed chrome specific apis + support firefox

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,33 +5,33 @@ const updateAllValues = factor => {
   });
 };
 
-chrome.extension.sendMessage({}, function(response) {
-  const readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-
-      // ----------------------------------------------------------
-      // This part of the script triggers when page is done loading
-      // ----------------------------------------------------------
-      let factor = 1;
-      const ingredients = document.getElementsByClassName("ingredient");
-      Array.prototype.map.call(ingredients, i => {
-        if (!i.innerText.includes("q.b.")) {
-          const re = /(\d+)/;
-          const recipeQty = re.exec(i.innerHTML)[0];
-          i.innerHTML = i.innerHTML.replace(
-            recipeQty,
-            `<input type="number" value="${recipeQty}" data-ogvalue="${recipeQty}" class="input-qty">`
-          );
-        }
-      });
-      const inputs = document.getElementsByClassName("input-qty");
-      Array.prototype.map.call(inputs, i => {
-        i.addEventListener("change", () => {
-          factor = i.value / i.dataset.ogvalue;
-          updateAllValues(factor);
-        });
-      });
+const main = () => {
+  let factor = 1;
+  const ingredients = document.getElementsByClassName("ingredient");
+  Array.prototype.map.call(ingredients, i => {
+    if (!i.innerText.includes("q.b.")) {
+      const re = /(\d+)/;
+      const recipeQty = re.exec(i.innerHTML)[0];
+      i.innerHTML = i.innerHTML.replace(
+        recipeQty,
+        `<input type="number" value="${recipeQty}" data-ogvalue="${recipeQty}" class="input-qty">`
+      );
     }
-  }, 10);
-});
+  });
+  const inputs = document.getElementsByClassName("input-qty");
+  Array.prototype.map.call(inputs, i => {
+    i.addEventListener("change", () => {
+      factor = i.value / i.dataset.ogvalue;
+      updateAllValues(factor);
+    });
+  });
+};
+
+const checkIfReady = () => {
+  if (document.readyState !== "interactive") {
+    checkIfReady();
+  }
+  main();
+};
+
+checkIfReady();
