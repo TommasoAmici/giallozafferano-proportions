27827b98e88dbcd86ad4cc8d12c86abe6a37c57e
Tommaso A <tommaso.amici@me.com>
2019-01-29 15:16:41 +0100

    v 0.0.1

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,37 @@
+const updateAllValues = factor => {
+  const inputs = document.getElementsByClassName("input-qty");
+  Array.prototype.map.call(inputs, i => {
+    i.value = i.dataset.ogvalue * factor;
+  });
+};
+
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  const readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-	}
-	}, 10);
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      // ----------------------------------------------------------
+      let factor = 1;
+      const ingredients = document.getElementsByClassName("ingredient");
+      Array.prototype.map.call(ingredients, i => {
+        if (!i.innerText.includes("q.b.")) {
+          const re = /(\d+)/;
+          const recipeQty = re.exec(i.innerHTML)[0];
+          i.innerHTML = i.innerHTML.replace(
+            recipeQty,
+            `<input type="number" value="${recipeQty}" data-ogvalue="${recipeQty}" class="input-qty">`
+          );
+        }
+      });
+      const inputs = document.getElementsByClassName("input-qty");
+      Array.prototype.map.call(inputs, i => {
+        i.addEventListener("change", () => {
+          factor = i.value / i.dataset.ogvalue;
+          updateAllValues(factor);
+        });
+      });
+    }
+  }, 10);
 });
